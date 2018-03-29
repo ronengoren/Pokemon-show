c70c93d9b7c141bbbeb8522c22eb8cbe5b0dd15f
ronen.goren <ronen.goren@gmail.com>
2018-03-29 17:32:13 -0400

    updatemarch29

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+
+
+
 class Pokemon {
   constructor(id, name, hp, attack, defense, abilities, types) {
   this.name = name;
@@ -42,344 +45,122 @@ let pokemonData = {
 }
 
 
+//Getting all informatiom from API when each function represrnt a different idnumber of the pokemon
 
 let getPokemonData = function(idNumer){
-  $.ajax({
+  return $.ajax({
     method: 'GET',
     dataType: 'json',
     url: 'https://pokeapi.co/api/v2/pokemon' + idNumer,
     success: function(data){
-      // console.log(data)
+      console.log(data)
       pokemonData.name = data.name;
       pokemonData.id = data.id;
-      pokemonData.defensTitle = data.stats[3].stat.name;
       pokemonData.defense = data.stats[3].base_stat;
-      pokemonData.attackTitle = data.stats[4].stat.name;
       pokemonData.attack = data.stats[4].base_stat;
-      pokemonData.hpTitle = data.stats[5].stat.name;
       pokemonData.hp = data.stats[5].base_stat;
-      var abilities = data.abilities;
-    $.each(abilities, function(key, value) {
-        var abilitiesList = value.ability.name;
-        // if (url == 'https://pokeapi.co/api/v2/pokemon/31/') {
-        //
-        //   return
-        //   $("#nidoqueenAbilitiesInfoAbilitiesInfo").append("<li>"  + abilitiesList + "</li>");
-        //   $("#nidoqueenAbilitiesInfoAbilitiesInfo").show;
-        // }
-      });
+      pokemonData.abilities = data.abilities;
+      pokemonData.types = data.types;
 
-        // console.log(abilitiesList)
-
-  // let types = data.types;
-
-// $.each(types, function(key, value) {
-//     let typesList = value.type.name;
-//     // console.log(typesList);
-//     // console.log(abilitiesList)
-//
-//   })
 
 
 },
+
 error: function() {
           alert('Error occurs!');
        }
 })
 }
 
-getPokemonData()
-
       let pokemon = new Pokemon(pokemonData);
 
-//     let pokeContainer = (pokemon) => {
-//     //div for all individual pokemon data
-//     //values pulled from Pokemon object and displayed in new HTML elements
-//
-//     let pokemonNumber = $("<h3/>").text(pokemon.number)
-//     let attack = $("<p/>").text("Attack: " + (pokemon.attack))
-//     let defense = $("<p/>").text("Defense: " + (pokemon.defense))
-//     let hp = $("<p/>").text("HP: " + (pokemon.hp))
-//     //create abilities list
-//     let abilitiesList = $("<ul/>").addClass("ability-list")
-//     pokemon.abilities.forEach((ability) => {
-//         if (ability.isHidden) {
-//             $("<li/>").text(ability.name).addClass("hidden-ability").appendTo(abilitiesList)
-//         }
-//         $("<li/>").text(ability.name).appendTo(abilitiesList)
-//     })
-//
-//
-//         // pokemonData.push(Pokemon)
-//         // console.log(pokemonData)
-//         //  console.log(pokemon.id);
-//         //  console.log(pokemon.id.name);
-//         //  console.log(pokemon.id.defensTitle);
-//         //  console.log(pokemon.id.defense);
-//         //  console.log(pokemon.id.attackTitle);
-//         //  console.log(pokemon.id.attack);
-//         //  console.log(pokemon.id.hpTitle);
-//         //  console.log(pokemon.id.hp);
-//
-//
-//
-// // })
-// }});
 
-// }
-
+// nidoqueen function works when click
 $("#nidoqueenimage").click(function(){
-  var url31 = "/31/"
-  var idNumer = url31
-    getPokemonData(idNumer)
+
+
+    getPokemonData("/31/").then(function(pokemonData){
     $("#nidoqueenTitle").text(pokemonData.name.toUpperCase());
     $("#nidoqueenReveal").text(pokemonData.name.toUpperCase());
-    $("#nidoqueenDefenceInfo").text("Defence: " + pokemonData.defense)
-    $("#nidoqueenAttackInfo").text("Attack: " + pokemonData.attack)
-    $("#nidoqueenHpInfo").text("Hp: " + pokemonData.hp)
+    $("#nidoqueenDefenceInfo").text("Defence: " + pokemonData.stats[3].base_stat)
+    $("#nidoqueenAttackInfo").text("Attack: " + pokemonData.stats[4].base_stat)
+    $("#nidoqueenHpInfo").text("Hp: " + pokemonData.stats[5].base_stat)
+    $("#nidoqueenabilitiesInfo").text("Abilities: " +pokemonData.abilities[0].ability.name + ", " + pokemonData.abilities[1].ability.name + ", " + pokemonData.abilities[2].ability.name)
+    $("#nidoqueentypesInfo").text("Types: " + pokemonData.types[0].type.name + ", " + pokemonData.types[1].type.name)
+    console.log(pokemonData.defense)
 
-  //   for ( let i = 0;  i < pokemonData.abilities.length; i++) {
-  //
-  // }
-    // for ( let i = 0;  i < pokemonData.abilities.length; i++) {
-    // 			 abilities.push(pokemonData.abilities[i].ability.name);
-    // 				console.log( okemonData.abilities[i].ability.name);
-    		// }
-      // $("#nidoqueenAbilitiesInfo").append("<li>"  + abilitiesList + "</li>");
-      // $("#nidoqueenAbilitiesInfo").show;
-
-
-
-    // let abilities = pokemonData.abilities;
-// $.each(abilities, function(key, value) {
-//       // let abilitiesList = value.ability.name;
-//     $("#nidoqueenAbilitiesInfo").text("Abilities: " + value.ability.name)
-
-
-      // console.log(pokemonData.abilities.ability)
-
-// })
-    // console.log(abilities)
-
-
-
-    // $(".nidoqueenRevealInfo").append("<p>" + pokemonData.attackTitle + ": " +pokemonData.attack +"</p>");
-    // $(".nidoqueenRevealInfo").append("<p>" + pokemonData.hpTitle + ": " +pokemonData.hp +"</p>");
-//     let abilities = pokemonData.abilities;
-// $.each(abilities, function(key, value) {
-//       let abilitiesList = value.ability.name;
-// $(".nidoqueenRevealInfo").append("<li>"  + value.ability.name + "</li>");
-// $(".nidoqueenRevealInfo").show;
-//
-//
-// });
+})
 })
 
-    // $("#nidoqueenRevealInfo").text(pokemonData.attackTitle + ": " +pokemonData.attack);
-    // $("#nidoqueenRevealInfo").text(pokemonData.defensTitle + ": " +pokemonData.defense);
-    // $("#nidoqueenRevealInfo").text(pokemonData.defensTitle + ": " +pokemonData.defense);
 
 
+// pidgeot function works when click
 
 
+    $("#pidgeotimage").click(function(){
+        getPokemonData("/18/").then(function(pokemonData){
+        $("#pidgeotTitle").text(pokemonData.name.toUpperCase());
+        $("#pidgeotReveal").text(pokemonData.name.toUpperCase());
+        $("#pidgeotDefenceInfo").text("Defence: " + pokemonData.stats[3].base_stat)
+        $("#pidgeotAttackInfo").text("Attack: " + pokemonData.stats[4].base_stat)
+        $("#pidgeotHpInfo").text("Hp: " + pokemonData.stats[5].base_stat)
+        $("#pidgeotabilitiesInfo").text("Abilities: " +pokemonData.abilities[0].ability.name + ", " + pokemonData.abilities[1].ability.name + ", " + pokemonData.abilities[2].ability.name)
+        $("#pidgeottypesInfo").text("Types: " + pokemonData.types[0].type.name + ", " + pokemonData.types[1].type.name)
 
-// console.log(pokemonData.name)
-
-
-$("#pidgeotimage").click(function(e){
-  e.preventDefault();
-  var url18 = "/18/"
-  var idNumer = url18
-    getPokemonData(idNumer)
-    $("#pidgeotTitle").text(pokemonData.name.toUpperCase());
-    $("#pidgeotReveal").text(pokemonData.name.toUpperCase());
-    $("#pidgeotDefenceInfo").text("Defence: " + pokemonData.defense)
-    $("#pidgeotAttackInfo").text("Attack: " + pokemonData.attack)
-    $("#pidgeotHpInfo").text("Hp: " + pokemonData.hp)
-
-    // $(".pidgeotRevealInfo").append("<p>" + pokemonData.defensTitle + ": " +pokemonData.defense +"</p>");
-    // $(".pidgeotRevealInfo").append("<p>" + pokemonData.attackTitle + ": " +pokemonData.attack +"</p>");
-    // $(".pidgeotRevealInfo").append("<p>" + pokemonData.hpTitle + ": " +pokemonData.hp +"</p>");
-//     let abilities = pokemonData.abilities;
-// $.each(abilities, function(key, value) {
-//       let abilitiesList = value.ability.name;
-// $(".pidgeotRevealInfo").append("<li>"  + value.ability.name + "</li>");
-// $(".pidgeotRevealInfo").show;
-//
-// });
+    })
+})
+// arbok function works when click
 
 
-  });
-// console.log(pokemonData.name)
+      $("#arbokimage").click(function(){
+          getPokemonData("/24/").then(function(pokemonData){
+          $("#arbokTitle").text(pokemonData.name.toUpperCase());
+          $("#arbokReveal").text(pokemonData.name.toUpperCase());
+          $("#arbokDefenceInfo").text("Defence: " + pokemonData.stats[3].base_stat)
+          $("#arbokAttackInfo").text("Attack: " + pokemonData.stats[4].base_stat)
+          $("#arbokHpInfo").text("Hp: " + pokemonData.stats[5].base_stat)
+          $("#arbokabilitiesInfo").text("Abilities: " +pokemonData.abilities[0].ability.name + ", " + pokemonData.abilities[1].ability.name + ", " + pokemonData.abilities[2].ability.name)
+          $("#arboktypesInfo").text("Types: " + pokemonData.types[0].type.name)
 
-$("#arbokimage").click(function(){
-  var url24 = "/24/"
-  var idNumer = url24
-    getPokemonData(idNumer)
-    $("#arbokTitle").text(pokemonData.name.toUpperCase());
-    $("#arbokReveal").text(pokemonData.name.toUpperCase());
-    $("#arbokDefenceInfo").text("Defence: " + pokemonData.defense)
-    $("#arbokAttackInfo").text("Attack: " + pokemonData.attack)
-    $("#arbokHpInfo").text("Hp: " + pokemonData.hp)
-//     let abilities = pokemonData.abilities;
-// $.each(abilities, function(key, value) {
-//       let abilitiesList = value.ability.name;
-// $(".arbokRevealInfo").append("<li>"  + value.ability.name + "</li>");
-// $(".arbokRevealInfo").show;
-//
-// });
+        });
+})
+
+$("#slakothimage").click(function(){
+
+    getPokemonData("/287/").then(function(pokemonData){
+    $("#slakothTitle").text(pokemonData.name.toUpperCase());
+    $("#slakothReveal").text(pokemonData.name.toUpperCase());
+    $("#slakothDefenceInfo").text("Defence: " + pokemonData.stats[3].base_stat)
+    $("#slakothAttackInfo").text("Attack: " + pokemonData.stats[4].base_stat)
+    $("#slakothHpInfo").text("Hp: " + pokemonData.stats[5].base_stat)
+    $("#slakothabilitiesInfo").text("Abilities: " +pokemonData.abilities[0].ability.name + ", " + pokemonData.abilities[1].ability.name + ", " + pokemonData.abilities[2].ability.name)
+    $("#slakothtypesInfo").text("Types: " + pokemonData.types[0].type.name)
 
   });
-// console.log(pokemonData.name)
+})
+$("#clefairyimage").click(function(){
 
-      // console.log(idNumer);
-      // console.log(name);
-      // console.log(defensTitle);
-      // console.log(defense);
-      // console.log(attackTitle);
-      // console.log(attack);
-      // console.log(hpTitle);
-      // console.log(hp);
-//
-//
-//
-// // }});
-// $.ajax({
-//   method: 'GET',
-//   dataType: 'json',
-//   url: 'https://pokeapi.co/api/v2/pokemon/24/',
-//   success: function(data){
-//     console.log(data);
-//     let name = data.name;
-//     $("#name2").text(data.name)
-//     let defensTitle = data.stats[3].stat.name;
-//     $("#defence2").text(data.stats[3].stat.name + " : " + data.stats[3].base_stat)
-//     let defense = data.stats[3].base_stat;
-//     let attackTitle = data.stats[4].stat.name;
-//     $("#attack2").text(data.stats[4].stat.name + " : " + data.stats[4].base_stat)
-//
-//     let attack = data.stats[4].base_stat;
-//     let hpTitle = data.stats[5].stat.name;
-//     $("#hp2").text(data.stats[5].stat.name + " : " + data.stats[5].base_stat)
-//
-//     let hp = data.stats[5].base_stat;
-//     let abilities = data.abilities;
-// $.each(abilities, function(key, value) {
-//       let abilitiesList = value.ability.name;
-//       $("#abilitiesList2").append("<li>"  + abilitiesList + "</li>");
-//       $("#abilitiesList2").show;
-//        console.log(abilitiesList);
-//
-//     });
-// //
-//
-//
-//
-//
-// }});
-//
-// $.ajax({
-//   method: 'GET',
-//   dataType: 'json',
-//   url: 'https://pokeapi.co/api/v2/pokemon/18/',
-//   success: function(data){
-//     console.log(data);
-//     let name = data.name;
-//     $("#name3").text(data.name)
-//     let defensTitle = data.stats[3].stat.name;
-//     $("#defence3").text(data.stats[3].stat.name + " : " + data.stats[3].base_stat)
-//     let defense = data.stats[3].base_stat;
-//     let attackTitle = data.stats[4].stat.name;
-//     $("#attack3").text(data.stats[4].stat.name + " : " + data.stats[4].base_stat)
-//
-//     let attack = data.stats[4].base_stat;
-//     let hpTitle = data.stats[5].stat.name;
-//     $("#hp3").text(data.stats[5].stat.name + " : " + data.stats[5].base_stat)
-//
-//     let hp = data.stats[5].base_stat;
-//     let abilities = data.abilities;
-//
-//
-//
-// $.each(abilities, function(key, value) {
-//
-//       let abilitiesList = value.ability.name;
-//       $("#abilitiesList3").append("<li>"  + abilitiesList + "</li>");
-//       $("#abilitiesList3").show;
+    getPokemonData("/35/").then(function(pokemonData){
+    $("#clefairyTitle").text(pokemonData.name.toUpperCase());
+    $("#clefairyReveal").text(pokemonData.name.toUpperCase());
+    $("#clefairyDefenceInfo").text("Defence: " + pokemonData.stats[3].base_stat)
+    $("#clefairyAttackInfo").text("Attack: " + pokemonData.stats[4].base_stat)
+    $("#clefairyHpInfo").text("Hp: " + pokemonData.stats[5].base_stat)
+    $("#clefairyabilitiesInfo").text("Abilities: " +pokemonData.abilities[0].ability.name + ", " + pokemonData.abilities[1].ability.name + ", " + pokemonData.abilities[2].ability.name)
+    $("#clefairytypesInfo").text("Types: " + pokemonData.types[0].type.name)
 
+  });
+})
+$("#oddishimage").click(function(){
 
-    // });
-    // $.each(data, function(index) {
-    //   return index > 13;
-    //   console.log(data.findIndex(index));
-    //       //
-    //       // let abilitiesTitle = key.index;
-    //       // console.log(abilitiesTitle);
-    //
-    //
-    //     });
+    getPokemonData("/43/").then(function(pokemonData){
+    $("#oddishTitle").text(pokemonData.name.toUpperCase());
+    $("#oddishReveal").text(pokemonData.name.toUpperCase());
+    $("#oddishDefenceInfo").text("Defence: " + pokemonData.stats[3].base_stat)
+    $("#oddishAttackInfo").text("Attack: " + pokemonData.stats[4].base_stat)
+    $("#oddishHpInfo").text("Hp: " + pokemonData.stats[5].base_stat)
+    $("#oddishabilitiesInfo").text("Abilities: " +pokemonData.abilities[0].ability.name + ", " + pokemonData.abilities[1].ability.name + ", " + pokemonData.abilities[2].ability.name)
+    $("#oddishtypesInfo").text("Types: " + pokemonData.types[0].type.name)
 
-
-
-
-
-// }});
-
-//
-//
-// let name = $("#name");
-// let defence = $("defence");
-// // let picture = $("#profilePicture");
-// // let repositories = $("#repositories")
-// // let button = $("#button")
-// // // let repolist = $("#repolist")
-// // let repolist = $("#repolist")
-//
-//
-//
-//
-//
-
-
-
-// function abilitiesListfunc(){
-// for (var index = 0; index < abilitiesList.length;  index++)
-// {
-
-// }
-// }
-
-// abilitiesListfunc()
-
-// $("#abilities").text(value.ability.name)
-
-
-
-
-      // $.each(data, function(name, value){
-      //   $("#name").text(value.name);
-      //
-      // // $.each(data1, function(name, value){
-      // //   console.log(value.name)
-      //   // $("#defence").length(forms.stats[3].effort)
-      //
-      // })
-      // console.log(forms.stats[].effort)
-      //   $("#defence").text(value.name);
-
-
-
-      // }});
-      // // let avatar_url = data.avatar_url;
-      //   // console.log(data[1].forms)
-      //   // $("#profilePicture").html('<img src="' + data.avatar_url + '" />');
-      //   // $("#repositories").text(data.public_repos);
-      //   // $("#button").click(function(){
-      //   //   window.location = data.html_url;
-      //
-      //
-      //
-      //
-      //
-      //   // $("#repolist").text
-      // },
+  });
+})
